bbd98b68bf91727a2e8a6698a5c45d2e58c33273
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-30 14:22:01 -0500

    made comments of the sort array from Udayan

--- src/question2/SortTest.java
+++ src/question2/SortTest.java
@@ -1,3 +1,13 @@
+/**
+ * Question 2:
+ * Same as Question 1.
+ * java.util.Arrays class has various utility methods to work with Arrays.
+ * Instead of writing your own logic to sort the passed int [] object in the method,
+ * you have to use a method from java.util.Arrays class to solve it.
+ *
+ * @author Udayan Khattry
+ */
+
 package question2;
 
 public class SortTest {
@@ -6,6 +16,8 @@ public class SortTest {
         System.out.print("Before the sort: ");
         Sort.printArray(arr);
 
+
+        // Arrays.sort(arr); //Sort the array using java.util.Arrays.sort(int []) method. -- this is what Udayan did instead of creating a separate file to sort the array like I did
         Sort.sortArray(arr);
         System.out.print("After the sort: ");
         Sort.printArray(arr);
